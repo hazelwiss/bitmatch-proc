cf665535e96295384944901ae857768e31f4631a
Kim-Dewelski <>
2022-08-17 11:56:37 +0200

    overhaul.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,10 +1,7 @@
 use proc_macro2::{Ident, Span};
 use quote::{quote, ToTokens};
 use std::ops::Range;
-use syn::{
-    parse::{Parse, ParseStream},
-    Expr, LitStr, Token,
-};
+use syn::{parse::Parse, Expr, ExprMatch};
 
 #[proc_macro]
 pub fn bitmatch(stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -20,17 +17,34 @@ enum Type {
     U128,
 }
 
+impl Type {
+    fn type_name(&self) -> proc_macro2::TokenStream {
+        match self {
+            Type::U8 => quote!(u8),
+            Type::U16 => quote!(u16),
+            Type::U32 => quote!(u32),
+            Type::U64 => quote!(u64),
+            Type::U128 => quote!(u128),
+        }
+    }
+}
+
 struct Var {
     range: Range<usize>,
     ident: char,
 }
 
-struct Arm {
-    kind: Type,
-    condition_mask: u128,
-    condition_val: u128,
-    vars: Vec<Var>,
-    body: Expr,
+enum Arm {
+    Pattern {
+        kind: Type,
+        condition_mask: u128,
+        condition_val: u128,
+        vars: Vec<Var>,
+        body: Expr,
+    },
+    Wild {
+        body: Expr,
+    },
 }
 
 struct Match {
@@ -41,15 +55,14 @@ struct Match {
 impl Parse for Match {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let mut arms = vec![];
-        input.parse::<Token![match]>()?;
-        let input_expr = input.parse::<Expr>()?;
-        let content;
-        syn::braced!(content in input);
-        while !content.is_empty() {
-            let arm = input.parse::<Arm>()?;
-            arms.push(arm);
+        let mexpr = input.parse::<ExprMatch>()?;
+        for arm in mexpr.arms {
+            arms.push(Arm::from_match_arm(arm));
         }
-        Ok(Self { arms, input_expr })
+        Ok(Self {
+            arms,
+            input_expr: *mexpr.expr,
+        })
     }
 }
 
@@ -65,12 +78,19 @@ impl ToTokens for Match {
     }
 }
 
-impl Parse for Arm {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let string = input
-            .parse::<LitStr>()
-            .or_else(|_| Err(input.error("failed to parse arm as string")))?;
-        let string = string.value();
+impl Arm {
+    fn from_match_arm(arm: syn::Arm) -> Self {
+        let string = match &arm.pat {
+            syn::Pat::Lit(patlit) => match &*patlit.expr {
+                Expr::Lit(expr) => match &expr.lit {
+                    syn::Lit::Str(lit_str) => lit_str.value(),
+                    _ => panic!("only accept string literals"),
+                },
+                _ => panic!("Unable to take in any expressions apart from a string literal"),
+            },
+            syn::Pat::Wild(_) => return Self::Wild { body: *arm.body },
+            _ => panic!("requires match arm to be a string literal or wild (_)"),
+        };
         let string_len = string.len();
         let kind = if string_len <= 8 {
             Type::U8
@@ -83,16 +103,16 @@ impl Parse for Arm {
         } else if string_len <= 128 {
             Type::U128
         } else {
-            return Err(input.error("string larger than any integer type can fit"));
+            panic!("string larger than any integer type can fit");
         };
-        let mut iter = string.chars().peekable();
+        let mut iter = string.chars().rev().peekable();
         let mut vars = vec![];
         let mut mask = 0;
         let mut val = 0;
         let mut index = 0;
         while let Some(c) = iter.next() {
             let bit = 1 << index;
-            index += 1;
+            let mut inc_index = true;
             match c {
                 '0' => mask |= bit,
                 '1' => {
@@ -100,92 +120,107 @@ impl Parse for Arm {
                     val |= bit
                 }
                 '?' => {}
-                '_' => index -= 1,
+                '_' => inc_index = false,
                 'a'..='z' => {
                     let begin = index;
                     let ident = c;
                     let mut size = 1;
                     if vars.iter().find(|var: &&Var| var.ident == ident).is_some() {
-                        return Err(input.error("duplicate variables"));
+                        panic!("duplicate variables");
                     }
                     while let Some(peek) = iter.peek() {
                         if *peek == ident {
                             size += 1;
+                            iter.next();
+                        } else if *peek == '_' {
+                            iter.next();
+                            continue;
                         } else {
                             break;
                         }
                     }
+                    index += size;
+                    inc_index = false;
                     vars.push(Var {
                         ident,
                         range: begin..begin + size,
                     })
                 }
-                _ => return Err(input.error("invalid character in bitmatch string pattern")),
+                _ => panic!("invalid character in bitmatch string pattern"),
+            }
+            if inc_index {
+                index += 1;
             }
         }
-        input.parse::<Token![=>]>()?;
-        let body = input.parse::<Expr>()?;
-        let _ = input.parse::<Token![,]>();
-        Ok(Self {
+        let body = *arm.body;
+        Self::Pattern {
             body,
             condition_mask: mask,
             condition_val: val,
             vars,
             kind,
-        })
+        }
     }
 }
 
 impl ToTokens for Arm {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let Arm {
-            kind,
-            condition_mask,
-            condition_val,
-            vars,
-            body,
-            ..
-        } = self;
-        let condition_mask = *condition_mask;
-        let condition_val = *condition_val;
-        let (condition_val_quote, condition_mask_quote) = match kind {
-            Type::U8 => {
-                let val = condition_val as u8;
-                let mask = condition_mask as u8;
-                (quote! {#val}, quote!(#mask))
-            }
-            Type::U16 => {
-                let val = condition_val as u16;
-                let mask = condition_mask as u16;
-                (quote! {#val}, quote!(#mask))
-            }
-            Type::U32 => {
-                let val = condition_val as u32;
-                let mask = condition_mask as u32;
-                (quote! {#val}, quote!(#mask))
-            }
-            Type::U64 => {
-                let val = condition_val as u64;
-                let mask = condition_mask as u64;
-                (quote! {#val}, quote!(#mask))
-            }
-            Type::U128 => (quote! {#condition_val}, quote! {#condition_mask}),
-        };
-        let var_initalization: Vec<proc_macro2::TokenStream> = vars
-            .iter()
-            .map(|var| {
-                let shft = var.range.start;
-                let mask = (1 << var.range.len()) - 1;
-                let ident = Ident::new(&String::from(var.ident), Span::call_site());
+        tokens.extend(match self {
+            Arm::Pattern {
+                kind,
+                condition_mask,
+                condition_val,
+                vars,
+                body,
+            } => {
+                let condition_mask = *condition_mask;
+                let condition_val = *condition_val;
+                let (condition_val_quote, condition_mask_quote) = match kind {
+                    Type::U8 => {
+                        let val = condition_val as u8;
+                        let mask = condition_mask as u8;
+                        (quote! {#val}, quote!(#mask))
+                    }
+                    Type::U16 => {
+                        let val = condition_val as u16;
+                        let mask = condition_mask as u16;
+                        (quote! {#val}, quote!(#mask))
+                    }
+                    Type::U32 => {
+                        let val = condition_val as u32;
+                        let mask = condition_mask as u32;
+                        (quote! {#val}, quote!(#mask))
+                    }
+                    Type::U64 => {
+                        let val = condition_val as u64;
+                        let mask = condition_mask as u64;
+                        (quote! {#val}, quote!(#mask))
+                    }
+                    Type::U128 => (quote! {#condition_val}, quote! {#condition_mask}),
+                };
+                let kind_quote = kind.type_name();
+                let var_initalization: Vec<proc_macro2::TokenStream> = vars
+                    .iter()
+                    .map(|var| {
+                        let shft = var.range.start;
+                        let mask = (1u128 << var.range.len()) - 1;
+                        let ident = Ident::new(&String::from(var.ident), Span::call_site());
+                        quote! {
+                            let #ident = ((_bitmatch as #kind_quote) >> #shft) & (#mask as #kind_quote);
+                        }
+                    })
+                    .collect();
                 quote! {
-                    let #ident = (_bitmatch >> #shft) & #mask
+                    _bitmatch if (_bitmatch as #kind_quote) & #condition_mask_quote == #condition_val_quote => {
+                        #(#var_initalization)*
+                        #body
+                    }
                 }
-            })
-            .collect();
-        tokens.extend(quote! {
-            _bitmatch if _bitmatch & #condition_mask_quote == #condition_val_quote => {
-                #(#var_initalization);*
-                #body
+            }
+            Arm::Wild { body } => {
+                quote! {
+                    _ => #body
+                }
             }
         });
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,54 @@
+#![no_std]
+
 pub use macros::bitmatch;
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+extern crate self as bitmatch;
+
+#[cfg(test)]
+mod tests {
+    use macros::bitmatch;
+
+    #[test]
+    fn test_match() {
+        // 0000 ???? ???? <- None,
+        // 0001 xxxx ???? <- x
+        // 0010 0?0? 0?xx <- x
+        // 0011 xxxx xx?0 <- x
+        let tests_input: std::vec::Vec<(u32, Option<u16>)> = vec![
+            (0b0000_1100_0010, Some(0)),
+            (0b0000_0101_0000, Some(0)),
+            (0b0000_0000_0000, Some(0)),
+            (0b0001_1111_0000, Some(0b1111)),
+            (0b0001_1101_0101, Some(0b1101)),
+            (0b0010_0101_0011, Some(0b11)),
+            (0b0010_0101_0111, Some(0b11)),
+            (0b0010_0101_1011, None),
+            (0b0010_0100_1100, None),
+            (0b0010_0101_0100, Some(0b00)),
+            (0b0010_0101_0110, Some(0b10)),
+            (0b0011_0000_1100, Some(0b11)),
+            (0b0011_0000_1101, None),
+            (0b0011_1110_1100, Some(0b11_1011)),
+        ];
+        let mut test_case = 0;
+        for (i, r) in tests_input {
+            let val = bitmatch! {
+                match i {
+                    "0000_????_????" => Some(0),
+                    "0001_xxxx_????" => Some(x),
+                    "0010_0?0?_0?xx" => Some(x),
+                    "0011_xxxx_xx?0" => Some(x),
+                    _ => None,
+                }
+            };
+            assert!(
+                    r == val,
+                    "expected '{r:?}', got '{val:?}'\noriginal input: '{i:012b}'\nat test_case {test_case}"
+                );
+            test_case += 1;
+        }
+    }
+}
